0c5b7b9012732c4542d374ab5342cf2581ab3dca
SyruSTR <trgrusted@gmail.com>
2024-11-26 17:30:21 +0100

    rewrite input system for tests

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <limits.h>
 
 #include "Hashmap.h"
+#include <limits>
 
 using namespace Warehouse;
 int main() {
@@ -35,16 +36,34 @@ int main() {
         }
         else if(command == "-a") {
             Warehouse::Item * tmpItem = new Warehouse::Item();
-            char tmpStr[100];
-            printf("Enter the itemID, its a number between 0 and %ui: ", INT_MAX);
-            scanf("%d",&tmpItem->itemID);
-            printf("Enter the item name: ");
-            scanf("%99s", &tmpStr);
-            tmpItem->itemName = tmpStr;
-            printf("Enter the price number between 1 and %ui: ", INT_MAX);
-            scanf("\n%ui\n",&tmpItem->price);
-            printf("Enter the count number between 1 and %ui: ", INT_MAX);
-            scanf("\n%ui\n",&tmpItem->count);
+
+            std::cout << "Enter the itemID: ";
+            if (!(std::cin >> tmpItem->itemID)) {
+                std::cerr << "Invalid input for itemID!" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+            std::cin.ignore();
+
+            std::cout << "Enter the item name: ";
+            std::getline(std::cin, tmpItem->itemName);
+
+            std::cout << "Enter the price: ";
+            if (!(std::cin >> tmpItem->price)) {
+                std::cerr << "Invalid input for price!" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+
+            std::cout << "Enter the count: ";
+            if (!(std::cin >> tmpItem->count)) {
+                std::cerr << "Invalid input for count!" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
 
             if (!map->addItem(*tmpItem))
                 printf("Item with this name isnt exist\n");
